efa078ec07c4bfce9136d136b42019bfc29433dc
ViniDias1 <viniciusdvalanca@gmail.com>
2025-06-16 20:31:22 -0300

    Refactor: Update prompts in enviar_para_ollama for clarity and detail

--- app/main.py
+++ app/main.py
@@ -15,11 +15,17 @@ def converter_imagem_para_base64(caminho):
         return base64.b64encode(f.read()).decode("utf-8")
 
 def enviar_para_ollama(imagem_base64):
-    prompt = "Extraia todos os preços visíveis na imagem. Liste apenas os valores monetários, como R$ 9,99."
+    prompt1 = "Extraia todos os preços visíveis na imagem. Liste apenas os valores monetários, como R$ 9,99."
+
+    prompt2 = """ Your task is to work and the job of an professional OCR tool, analyzing an image containing
+a price tag and one or more products. You should extract
+the product’s NAME, NORMAL PRICE, DISCOUNTED PRICE, UNIT, BARCODE NUMBERS and DESCRIPTION
+(in PT-BR).
+"""    
 
     payload = {
         "model": MODEL,
-        "prompt": prompt,
+        "prompt": prompt2,
         "images": [imagem_base64]
     }
 
